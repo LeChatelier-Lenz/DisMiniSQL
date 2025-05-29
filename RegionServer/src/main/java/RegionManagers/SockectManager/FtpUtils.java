1c12c86744af8f68a9e4537ac135085627d64ba1
wuwuwu3125 <1917181641@qq.com>
2025-05-29 12:59:23 +0800

    Update FtpUtils.java

--- RegionServer/src/main/java/RegionManagers/SockectManager/FtpUtils.java
+++ RegionServer/src/main/java/RegionManagers/SockectManager/FtpUtils.java
@@ -87,6 +87,67 @@ public class FtpUtils {
         return false;
     }
 
+    public Boolean downLoadblankFile() {
+        login();
+        OutputStream os = null;
+        String ftpPath = "blank";
+        String savePath = "";
+
+        if (ftpClient != null) {
+            try {
+                if (!ftpClient.changeWorkingDirectory(ftpPath)) {
+                    System.out.println("/" + ftpPath + " 该目录不存在");
+                    return false;
+                }
+
+                ftpClient.enterLocalPassiveMode();
+                FTPFile[] ftpFiles = ftpClient.listFiles();
+
+                if (ftpFiles == null || ftpFiles.length == 0) {
+                    System.out.println("/" + ftpPath + " 该目录下无文件");
+                    return false;
+                }
+
+                // 创建本地保存目录（如果不存在）
+                File localDir = new File(savePath);
+                if (!localDir.exists()) {
+                    localDir.mkdirs();
+                }
+
+                for (FTPFile file : ftpFiles) {
+                    if (!file.isDirectory()) {
+                        String fileName = file.getName();
+                        File saveFile = new File(savePath + fileName);
+                        os = new FileOutputStream(saveFile);
+                        boolean success = ftpClient.retrieveFile(fileName, os);
+                        os.close();
+
+                        if (success) {
+                            System.out.println("成功下载文件：" + fileName);
+                        } else {
+                            System.out.println("下载失败：" + fileName);
+                        }
+                    }
+                }
+                System.out.println("成功下载全部空白表单");
+                return true;
+            } catch (IOException e) {
+                System.out.println("下载文件失败：" + e.getMessage());
+            } finally {
+                if (os != null) {
+                    try {
+                        os.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                closeConnect();
+            }
+        }
+        return false;
+    }
+
+
     public boolean downloadtableFile(String ftpPath, String targetIP, String fileName, String savePath) {
         login();
         OutputStream os = null;
@@ -183,6 +244,58 @@ public class FtpUtils {
         return flag;
     }
 
+    public boolean upLoadblankFile(String fileName) {
+        login();
+        String savePath = "blank";
+        boolean flag = false;
+        InputStream inputStream = null;
+
+        if (ftpClient != null) {
+            try {
+                File file = new File(fileName);
+                if (!file.exists()) {
+                    System.out.println("本地文件不存在：" + fileName);
+                    return false;
+                }
+
+                ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+                ftpClient.makeDirectory(savePath);
+                ftpClient.changeWorkingDirectory(savePath);
+                // 获取当前目录下的文件名列表
+                String remoteFileName = file.getName();
+                FTPFile[] files = ftpClient.listFiles(remoteFileName);
+
+                if (files != null && files.length > 0) {
+                    System.out.println("不需要上传，文件已存在：" + remoteFileName);
+                    flag = true;
+                } else {
+                    inputStream = new FileInputStream(file);
+                    boolean done = ftpClient.storeFile(remoteFileName, inputStream);
+                    if (done) {
+                        System.out.println("成功上传空白表单：" + remoteFileName);
+                        flag = true;
+                    } else {
+                        System.out.println("上传失败：" + remoteFileName);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                closeConnect();
+            }
+        }
+
+        return flag;
+    }
+
+
     public boolean upLoadtableFile(String fileName, String localIP, String savePath) {
         login();
         boolean flag = false;
@@ -235,23 +348,6 @@ public class FtpUtils {
         return true;
     }
 
-    public boolean deleteFile(String fileName, String filePath) {
-        login();
-        boolean flag = false;
-        if (ftpClient != null) {
-            try {
-                ftpClient.changeWorkingDirectory(filePath);
-                ftpClient.dele(fileName);
-                System.out.println("删除FTP文件： " + fileName);
-                flag = true;
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                closeConnect();
-            }
-        }
-        return flag;
-    }
     public boolean deleteFile(String fileName,String localIP ,String filePath) {
         login();
         boolean flag = false;
